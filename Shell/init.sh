cba56f7ca2d3388b360f1a565666ff27bc39f56a
Khang <kt36@njit.edu>
2023-10-25 15:42:48 +0300

    modification

--- Shell/init.sh
+++ Shell/init.sh
@@ -3,7 +3,7 @@ mkdir results/models/
 mkdir results/dict/
 mkdir results/logs/
 
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install wandb loguru numpy scipy rich tqdm matplotlib 
 pip install torchmetrics
 pip install opacus
-conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
