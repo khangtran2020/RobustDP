1b09aa39159e4cc219703e664ed0a83e5b7be0c7
Khang <kt36@njit.edu>
2023-10-25 15:51:12 +0300

    modification

--- Shell/init.sh
+++ Shell/init.sh
@@ -4,6 +4,7 @@ mkdir results/dict/
 mkdir results/logs/
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install -U scikit-learn
 pip install wandb loguru numpy scipy rich tqdm matplotlib 
 pip install torchmetrics
 pip install opacus
