3e3187dee511dd580add06d22703fb5a52036898
Khang <kt36@njit.edu>
2023-10-25 20:35:25 +0300

    modification

--- Runs/fgsm.py
+++ Runs/fgsm.py
@@ -53,6 +53,8 @@ def robust_eval_clean(args, model:torch.nn.Module, device:torch.device, te_loade
                 perturbed_data = fgsm_attack(data_denorm[j], radius[j], data_grad[j])
                 perturbed_data_normalized = transforms.Normalize((0.1307,), (0.3081,))(perturbed_data)
                 adv_data = torch.cat((adv_data, perturbed_data_normalized), dim=0)
+
+            print(f"Size of the data: {data.size()}, {adv_data.size()}")
             output = model(adv_data)
 
             # Check for success
