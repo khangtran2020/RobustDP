ae922924c2fb1633417f35d740557a44db5eb378
Khang <kt36@njit.edu>
2023-10-25 13:14:06 +0300

    modification

--- Models/utils.py
+++ Models/utils.py
@@ -16,7 +16,7 @@ def check_clipped(model:torch.nn.Module, clip:float):
     res = True
     with torch.no_grad():
         for n, p in model.named_parameters():
-            if ('weight' in n) & ((p.norm(p=2) - clip).abs().item() < 1e-5):
+            if ('weight' in n) & ((p.norm(p=2) - clip).abs().item() > 1e-5):
                 console.log(f'[bold][red] Failed initial clip check :x:: clip is {clip} and norm is {p.norm(p=2).item()}')
                 sys.exit()
     if res:
