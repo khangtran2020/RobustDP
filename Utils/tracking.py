4d725fe3f93efa6a4c970deef0f4bf2f52fc6520
Khang <kt36@njit.edu>
2023-10-31 13:26:30 +0300

    add pgd

--- Utils/tracking.py
+++ Utils/tracking.py
@@ -13,8 +13,8 @@ def tracker_log_table(dct:Dict, name:str):
     for key in dct.keys():
         data.append([f'{key}', f'{dct[key]}'])
     my_table = wandb.Table(columns=["key", "value"], data=data)
-    wandb.run.log({name: my_table})
+    wandb.log({name: my_table})
 
 def tracker_summary(dct:Dict):
     for key in dct.keys():
-        wandb.run.summary[key] = dct[key]
+        wandb.summary[key] = dct[key]
