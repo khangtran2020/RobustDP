bd12c8ec149b590a0e54bb6200d230e768a36702
Khang <kt36@njit.edu>
2023-11-07 11:59:47 +0300

    test model parameter size

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def run(args, date, device):
     data_hist, model_hist, att_hist = init_history(args=args)
     name = get_name(args=args, current_date=date)
     tr_loader, va_loader, te_loader = read_data(args=args)
-    model = CNN(channel=[32, 64], hid_dim=[256, 64], img_size=args.img_sz, channel_in=args.channel_in, out_dim=args.num_class, kernal_size=5, debug=args.debug)
+    model = CNN(channel=[32, 64], hid_dim=[256, 64], img_size=args.img_sz, channel_in=args.channel_in, out_dim=args.num_class, kernal_size=3, debug=args.debug)
     
     if args.debug > 0:
         for n, p in model.named_parameters():
