c13ee1391e26d9df48496bea45f3ada269501819
Khang <kt36@njit.edu>
2023-11-07 11:10:19 +0300

    test model parameter size

--- Attacks/utils.py
+++ Attacks/utils.py
@@ -38,7 +38,7 @@ def robust_eval_clean(args, model:torch.nn.Module, device:torch.device, te_loade
             org_scores = model(data)
             top_k, idx = torch.topk(input=org_scores, k=num_c)
             wei = las_w[idx, :]
-            console.log(f"weight diff size: {wei.size()}")
+            console.log(f"weight diff size: {wei.size()}, since the idx has size: {idx.size()}")
             for j in range(1, num_c):
                 console.log(f"weight diff size: {(wei[0] - wei[j]).size()}")
                 M = (wei[0] - wei[j]).norm(p=2)
