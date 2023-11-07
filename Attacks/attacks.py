026e73f7c3a592458827590cc0bd056bc744a48e
Khang <kt36@njit.edu>
2023-11-07 11:42:38 +0300

    test model parameter size

--- Attacks/attacks.py
+++ Attacks/attacks.py
@@ -37,8 +37,9 @@ def pgd_attack(image:torch.Tensor, label:torch.Tensor, steps:int, model:torch.nn
         delta = adv - img
         weight = rad / (torch.flatten(delta, start_dim=1).norm(p=2, dim=1) + 1e-12)
         weight = torch.min(torch.ones_like(weight), weight)
-        console.log(f"weight: {weight.max()}, with size: {weight.size()}")
-        delta = delta * torch.min(1.0, rad / (torch.flatten(delta, start_dim=1).norm(p=2, dim=1) + 1e-12)).unsqueeze(dim=1).repeat(1, int(img.numel() / bsz)).view(img.size())
+        console.log(f"weight: max {weight.max()}, min {weight.min()}, with size: {weight.size()}")
+        for j in range(delta.size(dim=0)):
+            delta[j] = delta[j] * weight[j]
         adv = torch.clamp(img + delta, min=0, max=1).detach()
 
     return adv
