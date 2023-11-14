291b9076f5bdcad8179fe1d0bb7feb11f99dfadc
Khang <kt36@njit.edu>
2023-11-14 13:12:57 +0300

    robust eval for dp

--- Runs/dpsgd.py
+++ Runs/dpsgd.py
@@ -147,6 +147,7 @@ def traindp(args, tr_loader:torch.utils.data.DataLoader, va_loader:torch.utils.d
             if (epoch == args.epochs - 1):
                 console.log(f"# of model: {len(model_list)}")
                 for i, m in enumerate(model_list):
+                    print(m)
                     torch.save(m.state_dict(), args.model_path + f"model_{i+1}_{model_name}")
                     m.eval()
 
