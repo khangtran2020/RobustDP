a8260d6c410238689c444f4b29963d90aeadfe97
Khang <kt36@njit.edu>
2023-11-19 19:42:24 +0300

    add pretrained model

--- Models/utils.py
+++ Models/utils.py
@@ -88,8 +88,8 @@ def init_model(args):
         model = CNN(channel=[32, 64], hid_dim=[256, 64], img_size=args.img_sz, channel_in=args.channel_in, out_dim=args.num_class, kernal_size=3, debug=args.debug)
     elif args.model == 'vgg16':
         model = vgg16(weights=torchvision.models.VGG16_Weights.IMAGENET1K_V1)
-        last_in = model.fc.in_features
-        model.fc = torch.nn.Linear(last_in, args.num_class)
+        last_in = model.classifier[-1].in_features
+        model.classifier[-1] = torch.nn.Linear(last_in, args.num_class)
     elif args.model == 'vgg19':
         model = vgg19(weights=torchvision.models.VGG19_Weights.IMAGENET1K_V1)
         last_in = model.fc.in_features
