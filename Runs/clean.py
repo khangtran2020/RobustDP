d56437926e854ae56b67212bf2cf63152213a6d4
Khang <kt36@njit.edu>
2023-11-22 11:28:49 +0300

    add utk

--- Runs/clean.py
+++ Runs/clean.py
@@ -11,8 +11,8 @@ def train(args, tr_loader:torch.utils.data.DataLoader, va_loader:torch.utils.dat
     
     model_name = '{}.pt'.format(name)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, 
-                                                        #    threshold_mode='rel',cooldown=0, min_lr=0, eps=1e-08)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, 
+                                                           threshold_mode='rel',cooldown=0, min_lr=0, eps=1e-08)
 
     if args.num_class > 1:
         objective = torch.nn.CrossEntropyLoss().to(device)
@@ -87,7 +87,7 @@ def train(args, tr_loader:torch.utils.data.DataLoader, va_loader:torch.utils.dat
             va_perf = metrics.compute().item()
             metrics.reset()
 
-            # scheduler.step(metrics=va_loss)
+            scheduler.step(metrics=va_loss)
 
             results = {
                 "Target train/loss": tr_loss, 
