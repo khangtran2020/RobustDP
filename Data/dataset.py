c1d15647dbd3f3aaf1e74ef5b6718a746f093947
Khang <kt36@njit.edu>
2023-11-22 11:52:53 +0300

    add utk

--- Data/dataset.py
+++ Data/dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 class Data(Dataset):
     def __init__(self, X, y):
         self.X = torch.from_numpy(X.astype(np.float32))
-        self.y = torch.from_numpy(y.astype(np.float32))
+        self.y = torch.from_numpy(y.astype(np.float32)).long()
         self.len = self.X.shape[0]
 
     def __getitem__(self, index):
